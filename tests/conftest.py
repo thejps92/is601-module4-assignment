195608e3951dcc93dcac09c79434cd69d05c2644
John Scognomillo <jps92@njit.edu>
2026-02-15 23:25:03 -0500

    Add additional pytest configuration files

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from app.calculation import CalculationFactory
+from app.calculation import (
+    AddCalculation,
+    SubtractCalculation,
+    MultiplyCalculation,
+    DivideCalculation,
+    
+)
+
+@pytest.fixture(autouse=True)
+def reset_calculation_factory():
+    """
+    Fixture to reset CalculationFactory's registered calculations before each test.
+    """
+    CalculationFactory._calculations.clear()
+
+    CalculationFactory.register_calculation('add')(AddCalculation)
+    CalculationFactory.register_calculation('subtract')(SubtractCalculation)
+    CalculationFactory.register_calculation('multiply')(MultiplyCalculation)
+    CalculationFactory.register_calculation('divide')(DivideCalculation)
